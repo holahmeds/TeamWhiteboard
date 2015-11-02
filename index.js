b79bbfd9d882b5e93623734f6a2467dfd0a3b26d
Ahmed Rafiq <ahmedrafiq47@gmail.com>
2015-11-03 00:09:03 +0600

    Loud and Clear
    Added the ability for clients to communicate with the server. The
    Server also now new sends draw events to each client who now draw them.

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/room.html');
 });
 
+var userColor = new Array();
 io.on('connection', function(socket) {
-    console.log("A user connected");
-    
-    socket.on('disconnect', function() {
-        console.log("A user disconnected");
-    });
+	console.log("A user connected");
+
+	userColor[socket] = '#'
+			+ Math.floor(Math.random() * Math.pow(2, 24)).toString(16);
+
+	socket.emit('setColor', userColor[socket]);
+
+	socket.on('disconnect', function() {
+		console.log("A user disconnected");
+		delete userColor[socket];
+	});
+
+	socket.on('remoteDraw', function(x1, y1, x2, y2) {
+		socket.broadcast.emit('remoteDraw', x1, y1, x2, y2);
+	});
 });
 
 http.listen(3000, function() {
